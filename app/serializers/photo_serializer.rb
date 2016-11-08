03c540a4147d5a8413fafaa7f8292ea9f813bb61
styletab <katherinetab@gmail.com>
2016-11-08 10:48:53 -0500

    Update Photo Serializer
    Updated the photo serializer to be associated with a spot and added
    an editable method.

--- app/serializers/photo_serializer.rb
+++ app/serializers/photo_serializer.rb
@@ -1,4 +1,9 @@
 class PhotoSerializer < ActiveModel::Serializer
-  attributes :id, :file_name, :photo_upload_url
+  attributes :id, :file_name, :photo_upload_url, :editable
   has_one :user
+  has_one :spot
+
+  def editable
+    scope == object.user
+  end
 end
