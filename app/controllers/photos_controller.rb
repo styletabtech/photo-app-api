06f59df2931213708b6bc69bc92d7a35a8112cf7
styletab <katherinetab@gmail.com>
2016-11-08 10:52:38 -0500

    Add spot_id to photo parms in controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -54,6 +54,6 @@ class PhotosController < ApplicationController
     end
 
     def photo_params
-      params.require(:photo).permit(:file_name, :user_id, :photo_upload)
+      params.require(:photo).permit(:file_name, :user_id, :photo_upload, :spot_id)
     end
 end
