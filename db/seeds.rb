7efac4744e6a45097863feebbd7de96e58720f0f
styletab <katherinetab@gmail.com>
2016-11-10 17:15:46 -0500

    blah

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create({
-    {email: 'test@test.com', password: '123', password_confirmation: nil}
-  })
+User.create([
+    {email: 'test@test.com', password: '123', password_confirmation: '123' },
+  ])
 
 spots = Spot.create([
               { name: 'Boston Public Garden',
