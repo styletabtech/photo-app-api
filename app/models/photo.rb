5cc838742ce1eb60f857a3b942d48d66fae82928
styletab <katherinetab@gmail.com>
2016-11-10 00:30:28 -0500

    add seed data

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,5 @@
 class Photo < ActiveRecord::Base
   belongs_to :spot, inverse_of: :photos
   belongs_to :upload, inverse_of: :photos
+  belongs_to :user
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,60 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+spots = Spot.create([
+              { name: 'Boston Public Garden',
+                address: '4 Charles St',
+                city: 'Boston',
+                state: 'MA',
+                zip: 02116,
+                description: 'The Public Garden is a large park located in the heart of Boston, MA, adjacent to the Boston Common.',
+                user_id: 1
+              },
+
+              { name: 'Times Square',
+                address: 'Times Square',
+                city: 'New York City',
+                state: 'NY',
+                zip: 10036,
+                description: 'Bustling destination in the heart of the Theater District known for bright lights, shopping & shows.',
+                user_id: 1
+              },
+
+              { name: 'Lincoln Memorial Reflecting Pool',
+                address: '2 Lincoln Memorial Circle NW',
+                city: 'Washington',
+                state: 'DC',
+                zip: 20024,
+                description: 'This huge, iconic pool on the National Mall reflects the Lincoln Memorial & the Washington Monument.',
+                user_id: 2
+              },
+
+              { name: 'Wynnwood Walls',
+                address: '2520 NW 2nd Ave',
+                city: 'Miami',
+                state: 'FL',
+                zip: 33127,
+                description: 'Unique outdoor destination featuring huge, colorful street murals by artists from around the globe.',
+                user_id: 2
+              },
+
+              { name: 'Cloud Gate',
+                address: 'AT&T Plaza',
+                city: 'Chicago',
+                state: 'IL',
+                zip: 60601,
+                description: 'Huge outdoor sculpture shaped like a bean & allowing for views from its many mirrored sides.',
+                user_id: 3
+              },
+
+              { name: 'Urban Light',
+                address: '5905 Wilshire Blvd',
+                city: 'Los Angeles',
+                state: 'CA',
+                zip: 90036,
+                description: 'Urban Light is a large-scale assemblage sculpture by Chris Burden. The installation consists of 202 restored street lamps from the 1920s and 1930s.',
+                user_id: 1
+              }
+          ])
