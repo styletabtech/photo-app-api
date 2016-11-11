b753b8e1b6a405783a25777b895a0f505c99e4d3
styletab <katherinetab@gmail.com>
2016-11-10 21:19:12 -0500

    Remove editable from upload serializer

--- app/serializers/upload_serializer.rb
+++ app/serializers/upload_serializer.rb
@@ -1,14 +1,10 @@
 class UploadSerializer < ActiveModel::Serializer
-  attributes :id, :image_url, :photos, :spots, :user, :editable
+  attributes :id, :image_url, :photos, :spots, :user
   # attributes :id, :image_url, :photos, :spots
   # has_one :user
 
   def user
     object.user_id
-  end
-
-  def editable
-    scope == object.user
   end
 
   def spots
